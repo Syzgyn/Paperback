b7ee2cfad79cacebfd277f6b1af1022ac93debce
Syzgyn <git@syzgyn.com>
2021-04-07 16:53:06 +0000

    Fix api 404 page

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/bootstrap', 'bootstrap');
-Route::view('/{path?}', 'app')->where('path', '.*');
+Route::view('/{path?}', 'app')->where('path', '^(?!api).*$');
